16bfd57bc0030dff866e222a8e26412480dedc38
Sd140 <sakshi.dhenge@spit.ac.in>
2022-12-08 23:07:37 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const path = require('path')
 const app = express();
 const hbs = require("hbs")
 var fs = require('fs');
+var bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+var session = require('express-session')
 const port = process.env.PORT || 3000;
-
 
 const static_path = path.join(__dirname,"public")
 const partials_path = path.join(__dirname,"../templates/partials")
@@ -13,6 +15,8 @@ require("./db/conn");
 const Register = require("./models/register")
 const Course = require("./models/courses")
 const { json } = require("express")
+const auth = require("./middleware/auth");
+const async = require('hbs/lib/async');
 
 
 app.use(express.static(static_path));
@@ -21,11 +25,23 @@ app.use(express.urlencoded({extended:false}))
 app.set("view engine", "hbs");
 app.set("views",template_path)
 hbs.registerPartials(partials_path)
-
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(
+  session({
+    key:'user_data',
+    secret : "random",
+    resave: false,
+    saveUninitialized:false,
+    cookie:{
+      expires:500000
+    }
+  })
+)
 
 // Home page
 app.get('/',(req,res)=>{
-  res.render("index")
+  res.render("index",{buttonname:"Login"})
 })
 
 // about page
@@ -44,36 +60,6 @@ app.get('/courses',async(req,res)=>{
     });
 })
 
-})
-
-// upload-course
-app.post('/upload',async(req,res)=>{
-  try{
-    const course_name = req.body.course_name
-    const fees = req.body.fees
-    const lectures = req.body.lectures
-    const language = req.body.language
-    const description = req.body.description
-    const level = req.body.level
-    const category = req.body.category
-
-    const obj = new Course({
-     course_name : course_name,
-     fees : fees,
-     lectures : lectures,
-     language : language,
-     description : description,
-     level : level,
-     category : category,
-    })
-    const courses = await obj.save()
-    res.status(201).redirect("/")
-  }
-
-  catch(error){
-    console.log(error)
-    res.status(400).send(error)
-  }
 })
 
 // contact
@@ -96,18 +82,26 @@ app.get('/instructor',(req,res)=>{
   res.render("instructor")
 })
 
-// login
-app.get('/login',(req,res)=>{
-  res.render("login")
+// my courses on account button
+app.get('/mycourse',auth.isLogin,(req,res)=>{
+  const user = req.session.user
+  res.render("courses",{buttonname:user.firstname})
 })
 
-// login veirifcation
+
+// login
+app.get('/login',(req,res)=>{
+  res.render("login",{buttonname:"Login"})
+})
+
+// login verification
 app.post("/login",async(req,res)=>{
-  try{
+    try{
     const username = req.body.username
     const password = req.body.password
     const user_data = await Register.findOne({username:username})
     if(user_data.password==password){
+      req.session.user = user_data
       res.status(201).redirect("/")
     }
     else{
@@ -188,6 +182,41 @@ app.get('/course-single',(req,res)=>{
 // upload-course
 app.get('/upload',(req,res)=>{
   res.render("upload")
+})
+
+// upload-course
+app.post('/upload',async(req,res)=>{
+  const user = req.session.user
+  if(user.profession=="teacher")
+  {try{
+    const course_name = req.body.course_name
+    const fees = req.body.fees
+    const lectures = req.body.lectures
+    const language = req.body.language
+    const description = req.body.description
+    const level = req.body.level
+    const category = req.body.category
+
+    const obj = new Course({
+     course_name : course_name,
+     fees : fees,
+     lectures : lectures,
+     language : language,
+     description : description,
+     level : level,
+     category : category,
+    })
+    const courses = await obj.save()
+    res.status(201).redirect("/")
+  }
+
+  catch(error){
+    console.log(error)
+    res.status(400).send(error)
+  }}
+  else{
+    res.send("Only teachers can upload courses !")
+  }
 })
 
 app.listen(port, () => {
